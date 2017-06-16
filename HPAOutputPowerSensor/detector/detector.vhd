22a1bb2004b36e635bcf8707b60acf2fe70b8d72
Khwezi Majola <khwezimajola@gmail.com>
2017-06-16 11:19:00 +0200

    Added the led indicator code & changed the input buffer length and logic

--- HPAOutputPowerSensor/detector/detector.vhd
+++ HPAOutputPowerSensor/detector/detector.vhd
@@ -36,14 +36,14 @@ entity detector is
     Port ( clk : in  STD_LOGIC;
            cs : in  STD_LOGIC;
            data_in : in  STD_LOGIC_VECTOR (15 downto 0);
-           led : out  STD_LOGIC);
+           led : out  STD_LOGIC (7 downto 0));
 end detector;
 
 architecture Behavioral of detector is
 
 type machine is (search, found); 
 signal state		 	: machine 								:= search;
-type INPUT_BUFFER_TYPE is array (0 to 3) of STD_LOGIC_VECTOR (7 downto 0);
+type INPUT_BUFFER_TYPE is array (0 to 2) of STD_LOGIC_VECTOR (7 downto 0);
 -- Sequence "BEEF"  
 signal input_buffer : INPUT_BUFFER_TYPE;
 
@@ -57,16 +57,16 @@ begin
 					when search =>
 					-- On the falling edge of cs shift old data along the buffer then read in new data
 						if falling_edge(cs) then 
-							for i in 3 to 2 loop
-								input_buffer(i) <= input_buffer(i-2);
-							end loop;
+							--for i in 3 to 2 loop
+								input_buffer(2) <= input_buffer(0);
+							--end loop;
 							
 							input_buffer(0) <= data_in(15 downto 8);
 							input_buffer(1) <= data_in(7 downto 0);
 					-- Else check pairs if equal to sequence
 					-- If pair equal set state to found and exit loop
 						else
-							for i in 0 to 2 loop
+							for i in 0 to 1 loop
 								if ((input_buffer(i) = x"BE") and 
 								(input_buffer(i + 1) = x"EF")) then
 									state <= found;
@@ -76,10 +76,10 @@ begin
 						end if;
 			
 					when found =>
-						-- Light up LED
+						led <= x"AA";
 						
 					when others =>
-						-- Light up specific LEDs
+						led <= x"81";
 			end case;
 		end if;
 		
