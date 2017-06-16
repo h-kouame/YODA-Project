f9510de04beb4016bcab3e49f3a6639fc53f4d2c
Khwezi Majola <khwezimajola@gmail.com>
2017-06-16 13:15:23 +0200

    Fixed the type error for led and fixed the falling_edge error

--- HPAOutputPowerSensor/detector/detector.vhd
+++ HPAOutputPowerSensor/detector/detector.vhd
@@ -36,7 +36,7 @@ entity detector is
     Port ( clk : in  STD_LOGIC;
            cs : in  STD_LOGIC;
            data_in : in  STD_LOGIC_VECTOR (15 downto 0);
-           led : out  STD_LOGIC (7 downto 0));
+           led : out  STD_LOGIC_VECTOR (7 downto 0));
 end detector;
 
 architecture Behavioral of detector is
@@ -57,15 +57,15 @@ begin
 					when search =>
 					-- On the falling edge of cs shift old data along the buffer then read in new data
 						if falling_edge(cs) then 
-							--for i in 3 to 2 loop
+							for i in 3 to 2 loop
 								input_buffer(2) <= input_buffer(0);
-							--end loop;
+							end loop;
 							
 							input_buffer(0) <= data_in(15 downto 8);
 							input_buffer(1) <= data_in(7 downto 0);
 					-- Else check pairs if equal to sequence
 					-- If pair equal set state to found and exit loop
-						else
+						end if;
 							for i in 0 to 1 loop
 								if ((input_buffer(i) = x"BE") and 
 								(input_buffer(i + 1) = x"EF")) then
@@ -73,7 +73,7 @@ begin
 									exit;
 								end if;
 							end loop;
-						end if;
+						--end if;
 			
 					when found =>
 						led <= x"AA";
