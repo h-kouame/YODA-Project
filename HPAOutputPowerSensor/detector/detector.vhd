a3a649601246e360a4cdcc37a33131e7d58c0771
Khwezi Majola <khwezimajola@gmail.com>
2017-06-23 00:08:15 +0200

    The last commit This is as good as its going to get The input buffer should finally work properly

--- HPAOutputPowerSensor/detector/detector.vhd
+++ HPAOutputPowerSensor/detector/detector.vhd
@@ -37,7 +37,7 @@ entity detector is
            cs : in  STD_LOGIC;
 			  btns : in STD_LOGIC := '0';
            data_in : in  STD_LOGIC_VECTOR (15 downto 0);
-			  ss : in STD_LOGIC_VECTOR (15 downto 0) := x"FFFF";
+			  ss : in STD_LOGIC_VECTOR (15 downto 0);
            led : out  STD_LOGIC_VECTOR (7 downto 0));
 end detector;
 
@@ -45,18 +45,20 @@ architecture Behavioral of detector is
 
 type machine is (setup, waiting, search, found); 
 signal state		 	: machine 								:= setup;
-type INPUT_BUFFER_TYPE is array (0 to 3) of STD_LOGIC_VECTOR (7 downto 0);
+type INPUT_BUFFER_TYPE is array (0 to 2) of STD_LOGIC_VECTOR (7 downto 0);
 -- Sequence "BEEF"  
 signal input_buffer : INPUT_BUFFER_TYPE;
 signal prev : STD_LOGIC_VECTOR (15 downto 0) := x"FFFF";
 
-shared variable buffer_index : integer range 0 to 2 := 0;
-shared variable word_index : integer range 0 to 8 := 0;
-shared variable num_inputs : integer range 0 to 4 := 0;
+ 
 
 begin
 
 	process(clk) is
+	variable buffer_index : integer range 1 to 2 := 1;
+	variable word_index : integer range 0 to 8 := 0;
+	variable num_inputs : integer range 0 to 4 := 0;
+	
 	begin
 	
 		if falling_edge(clk) then 
@@ -65,83 +67,111 @@ begin
 					-- During setup reset the LEDs and wait for the first CS falling edge
 					-- Move to waiting 
 					-- The first CS falling edge occurs as part of the pmodad setup and must be ignored
-					led <= x"00";
-					if falling_edge(cs) then
-					
-					if (ss < x"0003") then
-						prev <= x"0000";
-					else
-						prev <= x"FFFF";
-					end if;
-					
-						state <= waiting;
-					end if;
+						led <= x"00";
+						if falling_edge(cs) then
+						
+							if (ss < x"0003") then
+								prev <= x"0000";
+							else
+								prev <= x"FFFF";
+							end if;
+							
+							state <= waiting;
+						end if;
 					
 					when waiting =>
-					led <= x"00";
 					-- During waiting, check for falling edge on SS to transition to search state
-					if ((prev > x"0800") and (ss < x"0003")) then
-						prev <= x"0000";
-						state <= search;
-						buffer_index := 0;
-						word_index := 0;
-						num_inputs := 0;
-					else
-						prev <= x"FFFF";
-					end if;
+						led <= x"00";
+						
+--						if ((prev > x"0800") and (ss < x"0003")) then
+--							prev <= x"0000";
+--							state <= search;
+--						else
+--							prev <= x"FFFF";
+--						end if;
+						
+						if (ss < x"0003") then
+							prev <= x"0000";
+							if (prev > x"0800") then
+								state <= search;
+							end if;
+						else
+							prev <= x"FFFF";
+						end if;
 					
 					when search =>
 					-- On the falling edge of cs shift old data along the buffer then read in new data
 					-- Else check pairs if equal to sequence
 					-- If pair equal set state to found and exit loop
-					led <= x"00";
-						if falling_edge(cs) then
-							
-							if (word_index = 8) then
-								if (buffer_index = 3) then
-									buffer_index := 0;
-								else
-									buffer_index := buffer_index + 1;
-								end if;
-												
-								if (num_inputs < 4) then
-									num_inputs := num_inputs + 1;
-								end if;
-								word_index := 0;
+						led <= x"00";
+							if falling_edge(cs) then
+								-- If 8 bits shifted in then 
+								-- Change the buffer index
+								-- Shift value in buffer
+								if (word_index = 8) then
+									if (buffer_index = 2) then
+										buffer_index := 1;
+									else
+										buffer_index := buffer_index + 1;
+										input_buffer(0) <= input_buffer(2);
+									end if;
+									-- Count at least 3 inputs
+									if (num_inputs < 3) then
+										num_inputs := num_inputs + 1;
+									end if;
+									word_index := 0;
 
-							else								
-								word_index := word_index + 1;
-							end if;
-							
-							if (data_in >= x"800")then
-								--led <= x"80";
-								input_buffer(buffer_index) <= input_buffer(buffer_index)(input_buffer(buffer_index)'high - 1 downto 0) & '1';
-							else
-								--led <= x"01";
-								input_buffer(buffer_index) <= input_buffer(buffer_index)(input_buffer(buffer_index)'high - 1 downto 0) & '0';
-							end if;
-							
-						end if;
-						if (num_inputs = 4) then
-							for i in 0 to 2 loop
-								if ((input_buffer(i + 1) = x"FF") and 
-								(input_buffer(i) = x"00")) then
-									state <= found;
-									exit;
+								else								
+								-- Change word index
+									word_index := word_index + 1;
 								end if;
-							end loop;							
-						end if;
-						
+								-- Shift in the next bit. Set value to 1 or 0
+								if (data_in >= x"800")then
+									--led <= x"CC";
+									input_buffer(buffer_index) <= input_buffer(buffer_index)(input_buffer(buffer_index)'high - 1 downto 0) & '1';
+								else
+									--led <= x"A0";
+									input_buffer(buffer_index) <= input_buffer(buffer_index)(input_buffer(buffer_index)'high - 1 downto 0) & '0';
+								end if;
+								
+							end if;
+							-- If there are enough inputs then check the buffer
+							if (num_inputs = 3) then
+								for i in 0 to 1 loop
+									if ((input_buffer(i + 1) = x"FF") and 
+									(input_buffer(i) = x"00")) then
+										state <= found;
+										exit;
+									end if;
+								end loop;
+							elsif (num_inputs = 2) then
+								if ((input_buffer(2) = x"FF") and 
+									(input_buffer(1) = x"00")) then
+										state <= found;
+								end if;
+							end if;
+							--end if;
+							
+					
 					when found =>
+					-- When found
+					-- Check reset condition
+					-- Display value on LEDs
 						if (btns = '1') then
 							if (ss < x"0003") then
 								prev <= x"0000";
 							else
 								prev <= x"FFFF";
 							end if;
+							buffer_index := 1;
+							word_index := 0;
+							num_inputs := 0;
 							state <= waiting;
 						end if;
-						led <= x"AA";
+						
+						if (ss < x"0003") then
+							led <= x"AA";
+						end if;
 						
 					when others =>
 						led <= x"81";
